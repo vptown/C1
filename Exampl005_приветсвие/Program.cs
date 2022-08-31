20d0213388b815e2494e95409d73922616b0fad3
Sultanov Rustam <v.p.town@gmail.com>
2022-08-31 14:43:23 +0500

    Hello

---
+++ Exampl005_приветсвие/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else 
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
